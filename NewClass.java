af2ae000668621a5f047d5b0d99d65c62415d4b7
Windows <Windows@DESKTOP-SDM9H7D.localdomain>
2022-03-09 11:33:34 +0100

    prueba1

---
+++ NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadoragui;
+
+/**
+ *
+ * @author Windows
+ */
+public class NewClass {
+    
+}
